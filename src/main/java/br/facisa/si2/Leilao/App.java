0bbd90551653fb4d1899467309570d37a1176653
João B <joao.filho@si.unifacisa.edu.br>
2019-05-17 14:56:39 -0300

    first commit with errors

---
+++ src/main/java/br/facisa/si2/Leilao/App.java
@@ -0,0 +1,21 @@
+package br.facisa.si2.Leilao;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class App implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
