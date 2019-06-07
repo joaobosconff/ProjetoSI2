370152e574e5de60fd0ccc10c25d5120accf2acf
João B <joao.filho@si.unifacisa.edu.br>
2019-06-07 01:10:21 -0300

    finalizando metodo de fazer lance

--- src/main/java/br/facisa/si2/Leilao/api/controllers/MediadorController.java
+++ src/main/java/br/facisa/si2/Leilao/api/controllers/MediadorController.java
@@ -9,10 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.facisa.si2.Leilao.api.domains.Comprador;
 import br.facisa.si2.Leilao.api.domains.Lance;
 import br.facisa.si2.Leilao.api.domains.Lote;
-import br.facisa.si2.Leilao.api.domains.Mediador;
 import br.facisa.si2.Leilao.api.exceptions.RestException;
 import br.facisa.si2.Leilao.api.services.LanceService;
 import br.facisa.si2.Leilao.api.services.LoteService;
@@ -33,24 +31,9 @@ public class MediadorController {
 	private LoteService loteService;
 
 	@PostMapping("/lance")
-	public ResponseEntity<Lance> criarLance(@RequestBody Lance l) throws RestException {
-		Mediador mediadorUnico = mediadorService.getAll().get(0);
-
-		String msg = mediadorUnico.setLance(l);
-		if (msg == "vencedor") {
-			confirmarCompra(l.getCompradorFinal());
-		} else {			
-		mediadorUnico.setPrecoDoLance(l.getPrecoLance());
-		mediadorService.atualiza(mediadorUnico);
-		return new ResponseEntity<Lance>(lanceService.add(l), HttpStatus.CREATED);
+	public ResponseEntity<String> criarLance(@RequestBody Lance l) throws RestException {
+		return new ResponseEntity<String>(mediadorService.fazLance(l), HttpStatus.CREATED);
 		}
-	}
-	
-	private ResponseEntity<String> confirmarCompra(Comprador compradorFinal) {
-		return new ResponseEntity<String>("Confirmar compra", HttpStatus.OK);
-	}
-	
-	@PostMapping("/confirmar")
 
 	@PostMapping("/lote")
 	public ResponseEntity<Lote> comprarLote(@RequestBody Lote l) {
--- src/main/java/br/facisa/si2/Leilao/api/services/MediadorService.java
+++ src/main/java/br/facisa/si2/Leilao/api/services/MediadorService.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.facisa.si2.Leilao.api.abstracts.ServiceAbs;
+import br.facisa.si2.Leilao.api.domains.Comprador;
+import br.facisa.si2.Leilao.api.domains.Lance;
 import br.facisa.si2.Leilao.api.domains.Mediador;
+import br.facisa.si2.Leilao.api.exceptions.RestException;
 import br.facisa.si2.Leilao.api.repositories.MediadorRepository;
 
 @Service
@@ -13,6 +16,28 @@ public class MediadorService extends ServiceAbs<Mediador>{
 	@Autowired
 	public MediadorService(MediadorRepository repository) {
 		super(repository);
+	}
+	
+	@Autowired
+	private LanceService lanceService;
+	
+	
+	public String fazLance(Lance l) throws RestException {
+		Mediador mediadorUnico = this.getAll().get(0);
+		String msg = mediadorUnico.setLance(l);
+		if (msg == "vencedor") {
+			return confirmarCompra(l,mediadorUnico.getComprador().get(0));
+		} else {			
+		mediadorUnico.setPrecoDoLance(l.getPrecoLance());
+		this.atualiza(mediadorUnico);}
+		return "atualiza";
+	}
+		
+	
+	public String confirmarCompra(Lance l,Comprador comprador) throws RestException {
+		String msg = "O lance" + l.toString() + " foi vendido para" + comprador.getNome();
+		lanceService.deleta(l.getId());
+		return msg;
 	}
 	
 
